de3ce7ffc55aa3105af0d5963beb4a29dc6a0f4e
lblmdesk <loganbendev@gmail.com>
2024-12-12 13:32:22 +1300

    Challenge: The Variable Shop

---
+++ players-guide/Challenges/PartOne/TheVariableShop.cs
@@ -0,0 +1,49 @@
+
+namespace Challenges.PartOne;
+
+public class TheVariableShop
+{
+    public void Solution()
+    {
+
+        char c = 'a';
+        Console.WriteLine(c);
+        string s = "hi";
+        Console.WriteLine(s);
+        byte b = 2;
+        Console.WriteLine(b);
+        sbyte sb = -4;
+        Console.WriteLine(sb);
+        short sv = 15;
+        Console.WriteLine(sv);
+        ushort us = 192;
+        Console.WriteLine(us);
+        int i = 2;
+        Console.WriteLine(i);
+        uint ui = 15;
+        Console.WriteLine(ui);
+        float f = 12312.528F;
+        Console.WriteLine(f);
+        double d = 23259.23;
+        Console.WriteLine(d);
+        long l = 1512315;
+        Console.WriteLine(l);
+        ulong ul = 12312875129837;
+        Console.WriteLine(ul);
+        decimal de = .000000000000005123M;
+        Console.WriteLine(de);
+        bool bo = true;
+        Console.WriteLine(bo);
+        int binary_literal = 0b00011001;
+        Console.WriteLine(binary_literal);
+        int hex = 0xFF00FF;
+        Console.WriteLine(hex);
+
+    }
+
+    public void Returns()
+    {
+
+    }
+
+}
