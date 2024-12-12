e87193f27fe213af4a2c760d7047fd0b1e7696b3
lblmdesk <loganbendev@gmail.com>
2024-12-12 16:31:33 +1300

    Challenge: Buying Inventory

---
+++ players-guide/Challenges/PartOne/BuyingInventory.cs
@@ -0,0 +1,50 @@
+namespace Challenges.PartOne;
+
+public class BuyingInventory
+{
+    public void Solution()
+    {
+
+        // Prices
+        var menu = new Dictionary<string, Tuple<string, int>>
+        {
+            { "1", new Tuple<string, int>
+                    ("Rope", 10)
+            },
+            { "2", new Tuple<string, int>
+                    ("Torches", 15)
+            },
+            { "3", new Tuple<string, int>
+                    ("Climbing Equipment", 25)
+            },
+            { "4", new Tuple<string, int>
+                    ("Clean Water", 1)
+            },
+            { "5", new Tuple<string, int>
+                    ("Machete", 20)
+            },
+            { "6", new Tuple<string, int>
+                    ("Canoe", 200)
+            },
+            { "7", new Tuple<string, int>
+                    ("Food Supplies", 1)
+        }
+        };
+
+        Console.WriteLine("The following items are available: ");
+        foreach (var item in menu)
+        {
+            string index = item.Key;
+            (string itemName, int itemCost) = item.Value;
+
+            Console.WriteLine($"{index} - {itemName}");
+        }
+
+        Console.Write("What number do you want to see the price of: ");
+        var wantedItem = Console.ReadLine() ?? "";
+
+        Console.WriteLine($"{menu[wantedItem].Item1} costs {menu[wantedItem].Item2} gold");
+
+
+    }
+}
