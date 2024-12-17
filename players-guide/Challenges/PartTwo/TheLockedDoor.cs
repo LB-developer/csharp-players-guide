840ed9a1c38e45b06d8a0a9c04e6a06968d6962c
lblmdesk <loganbendev@gmail.com>
2024-12-17 22:36:22 +1300

    OOP - Challenge 2/?: The Locked Door

--- players-guide/Challenges/PartTwo/TheLockedDoor.cs
+++ players-guide/Challenges/PartTwo/TheLockedDoor.cs
@@ -13,7 +13,43 @@ public class Door
         _state = State.Locked;
         _status = Status.Closed;
         _passcode = passcode;
+    }
 
+    public string UnlockDoor()
+    {
+        if (this._status == Status.Open)
+        {
+            return "The door is already unlocked and open";
+        }
+        string response;
+
+        response = this._state switch
+        {
+            State.Unlocked => "The door is already unlocked.",
+            State.Locked =>
+                (GuessPasscode())
+                ? "You have unlocked the door!"
+                : "Incorrect passcode",
+            _ => throw new Exception()
+
+        };
+
+        return response;
+    }
+
+    private bool GuessPasscode()
+    {
+        int guess;
+
+        Console.Write("Enter the passcode: ");
+        int.TryParse(Console.ReadLine(), out guess);
+
+        return guess switch
+        {
+            _ when guess == this._passcode => true,
+            _ => false
+        };
+    }
     public enum State
     {
         Locked,
