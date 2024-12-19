104d702cee1e86afadf8c8b7bd12d77cb793fdea
lblmdesk <loganbendev@gmail.com>
2024-12-19 22:06:28 +1300

    OOP - Challenge : The Password Validator 2/?
    fix loop

--- players-guide/Challenges/PartTwo/PasswordValidator.cs
+++ players-guide/Challenges/PartTwo/PasswordValidator.cs
@@ -3,7 +3,22 @@ namespace Challenges.PartTwo;
 public class PasswordValidator
 {
 
-    public IsValid(string password)
+    public bool IsValid()
+    {
+        string password = "";
+
+        do
+        {
+            Console.Write("Enter a password for validation: ");
+            password = Console.ReadLine() ?? "";
+
+        } while (!CheckLength(password) && !CheckCasing(password));
+
+        Console.WriteLine("That password is valid!");
+        return true;
+    }
+
+    private bool CheckLength(string password)
     {
         int passwordLength = password.Length;
         if (passwordLength < 6 || passwordLength > 13)
@@ -12,6 +27,57 @@ public class PasswordValidator
             return false;
         }
 
+        return true;
+    }
+
+    private bool CheckCasing(string password)
+    {
+        string response = "";
+        int uppercaseLetters = 0;
+        int lowercaseLetters = 0;
+        int digits = 0;
+
+        foreach (var character in password)
+        {
+            if (char.IsUpper(character))
+            {
+                uppercaseLetters++;
+            }
+            else if (char.IsLower(character))
+            {
+                lowercaseLetters++;
+            }
+            else if (char.IsDigit(character))
+            {
+                digits++;
+            }
+
+        }
+
+        Console.WriteLine(uppercaseLetters);
+        Console.WriteLine(lowercaseLetters);
+        Console.WriteLine(digits);
+
+        if (uppercaseLetters >= 1 && lowercaseLetters >= 1 && digits >= 1)
+        {
+            return true;
+        }
+
+        if (uppercaseLetters < 1)
+        {
+            response += "A valid password needs at least 1 uppercase letter (A-Z) \n";
+        }
+        if (lowercaseLetters < 1)
+        {
+            response += "A valid password needs at least 1 lowercase letter (a-z) \n";
+        }
+        if (digits < 1)
+        {
+            response += "A valid password needs at least 1 digit (0-9) \n";
+        }
+
+        Console.WriteLine(response);
+        return false;
     }
 
 }
