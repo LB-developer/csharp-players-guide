e518c1c86ee21a9f6d8330016e77e69b9dce891d
lblmdesk <loganbendev@gmail.com>
2024-12-20 10:27:33 +1300

    OOP - Challenge : Rock Paper Scissors 6/?
    Swap player each round

--- players-guide/Challenges/RockPaperScissors/Game.cs
+++ players-guide/Challenges/RockPaperScissors/Game.cs
@@ -4,18 +4,30 @@ public class Game
 {
     public int PlayerOneScore { get; private set; } = 0;
     public int PlayerTwoScore { get; private set; } = 0;
+
     private int PlayerTurn = 1;
     private DecisionPasser _decisionPasser = new DecisionPasser();
 
-    public void StartPlaying(int currentPlayerTurn)
+    public void StartPlaying()
     {
+        PlayRound(PlayerTurn);
+    }
 
+    private void PlayRound(int currentPlayerTurn)
+    {
         Option playerDecision = _decisionPasser.GetDecision(PlayerTurn);
 
 
 
-        PlayerTurn++;
-        this.StartPlaying(PlayerTurn);
+        if (PlayerTurn == 1)
+        {
+            PlayerTurn = 2;
+        }
+        else
+        {
+            PlayerTurn = 1;
+        }
+        this.PlayRound(PlayerTurn);
     }
 
 
