40f42c00f6f710cca7b4ec630f435dee6581e4a7
lblmdesk <loganbendev@gmail.com>
2024-12-20 10:21:52 +1300

    OOP - Challenge : Rock Paper Scissors 3/?
    Setup DecisionPasser
    Get decision from player

---
+++ players-guide/Challenges/RockPaperScissors/DecisionPasser.cs
@@ -0,0 +1,45 @@
+namespace Challenges.RockPaperScissors;
+
+internal class DecisionPasser()
+{
+    List<Option> PlayerOneDecisions = new List<Option>();
+    List<Option> PlayerTwoDecisions = new List<Option>();
+
+    internal Option GetDecision(int playerTurn)
+    {
+        int i = 0;
+        int userSelection = 0;
+
+        Console.WriteLine("Choices------");
+        foreach (var item in Enum.GetValues<Option>())
+        {
+            Console.WriteLine($"{i}: {item}");
+            i++;
+        }
+
+        do
+        {
+            Console.Write("Please enter your choice (1-3): ");
+            int.TryParse(Console.ReadLine(), out userSelection);
+
+        } while (userSelection != 1 || userSelection != 2 || userSelection != 3);
+
+
+        switch (userSelection)
+        {
+            case 1:
+                this.NoteDecision(Option.Rock, playerTurn);
+                return Option.Rock;
+            case 2:
+                this.NoteDecision(Option.Paper, playerTurn);
+                return Option.Paper;
+            case 3:
+                this.NoteDecision(Option.Scissors, playerTurn);
+                return Option.Scissors;
+        }
+
+        throw new Exception();
+    }
+
+
+}
