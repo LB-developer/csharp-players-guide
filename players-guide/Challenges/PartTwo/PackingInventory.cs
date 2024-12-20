cb8adc900b6d8774763664cfac3e75523f158dbc
lblmdesk <loganbendev@gmail.com>
2024-12-20 16:38:17 +1300

    Inheritance - Challenge : Pack 1/?

---
+++ players-guide/Challenges/PartTwo/PackingInventory.cs
@@ -0,0 +1,79 @@
+namespace Challenges.PartTwo;
+
+public class Pack
+{
+
+    public readonly double WeightLimit = 7.00;
+    public readonly double VolumeLimit = 4.00;
+
+    public double CurrentWeight { get; private set; }
+    public double CurrentVolume { get; private set; }
+
+    List<InventoryItem> Items { get; set; } = new List<InventoryItem>();
+
+
+
+
+}
+
+public class InventoryItem
+{
+    public double _weight { get; init; }
+    public double _volume { get; init; }
+
+    public InventoryItem(double weight, double volume)
+    {
+        _weight = weight;
+        _volume = volume;
+    }
+
+}
+
+
+public class Arrow : InventoryItem
+{
+    public Arrow() : base(0.1, 0.05)
+    {
+    }
+
+}
+
+public class Bow : InventoryItem
+{
+    public Bow() : base(1, 4)
+    {
+    }
+
+}
+
+public class Rope : InventoryItem
+{
+    public Rope() : base(1, 1.5)
+    {
+    }
+
+}
+
+public class Water : InventoryItem
+{
+    public Water() : base(2, 3)
+    {
+    }
+
+}
+
+public class Food : InventoryItem
+{
+    public Food() : base(1, 0.5)
+    {
+    }
+
+}
+
+public class Sword : InventoryItem
+{
+    public Sword() : base(5, 3)
+    {
+    }
+
+}
