1fda93b2c8a484a0437eafeadc47c3c7162fefff
lblmdesk <loganbendev@gmail.com>
2024-12-23 18:41:55 +1300

    Boss Battle (refactor) : The Fountain Of Objects

--- players-guide/Challenges/TheFountainOfObjects/Game.cs
+++ players-guide/Challenges/TheFountainOfObjects/Game.cs
@@ -2,12 +2,15 @@ namespace Challenges.TheFountainOfObjects;
 
 public class Game
 {
-    private GameRoom[,] Rooms = new GameRoom[4, 4];
-    private Player Player = new Player(0, 0);
+    private GameRoom[,] Rooms;
+    private Player Player;
     private bool GameFinished = false;
 
     public Game()
     {
+        Rooms = new GameRoom[4, 4];
+        Player = new Player(0, 0);
+
         this.InitializeBoard();
         this.StartGame();
     }
@@ -30,14 +33,7 @@ public class Game
 
         while (!GameFinished)
         {
-            Console.WriteLine($"---------------------------");
-            Console.WriteLine($"You are the room at (Row={Player.YLocation}, Column={Player.XLocation})");
-
-            if (Player.XLocation != 0 && Player.YLocation == 0)
-                Console.WriteLine("You see light coming from the cavern entrance.");
-            else if (Player.XLocation == 2 && Player.YLocation == 0)
-                Console.WriteLine("You hear water dripping in this room. The Fountain of Objects is here!");
-
+            this.StartNewRound();
             Player.GetPlayerAction();
             if (this.Player.XLocation == 0 && this.Player.YLocation == 0 && this.Player.FountainEnabled)
             {
@@ -48,6 +44,17 @@ public class Game
 
         Console.WriteLine("The Fountain of Objects has been reactivated, and, you have escaped with your life!");
         Console.WriteLine("You win!");
+    }
+
+    private void StartNewRound()
+    {
+        Console.WriteLine($"---------------------------");
+        Console.WriteLine($"You are the room at (Row={Player.YLocation}, Column={Player.XLocation})");
+
+        if (this.Player.XLocation == 0 && this.Player.YLocation == 0)
+            Console.WriteLine("You see light coming from the cavern entrance.");
+        else if (this.Player.XLocation == 2 && this.Player.YLocation == 0)
+            Console.WriteLine("You hear water dripping in this room. The Fountain of Objects is here!");
     }
 
     enum GameRoom
@@ -92,70 +99,37 @@ internal class Player
         switch (option)
         {
             case "move west":
-                if (!this.MovePlayer(-1, 0))
-                {
-                    Console.WriteLine("You are at the edge and cannot move west, choose another move");
-                    return false;
-                }
-                else
-                {
-                    return true;
-                }
-
+                return this.MovePlayer(-1, 0, "west");
             case "move east":
-                if (!this.MovePlayer(1, 0))
-                {
-                    Console.WriteLine("You are at the edge and cannot move east, choose another move");
-                    return false;
-                }
-                else
-                {
-                    return true;
-                }
-
+                return this.MovePlayer(1, 0, "east");
             case "move north":
-                if (!this.MovePlayer(0, 1))
-                {
-                    Console.WriteLine("You are at the edge and cannot move north, choose another move");
-                    return false;
-                }
-                else
-                {
-                    return true;
-                }
+                return this.MovePlayer(0, 1, "north");
             case "move south":
-                if (!this.MovePlayer(0, -1))
-                {
-                    Console.WriteLine("You are at the edge and cannot move south, choose another move");
-                    return false;
-                }
-                else
-                {
-                    return true;
-                }
+                return this.MovePlayer(0, -1, "south");
             case "enable fountain":
                 if (this.XLocation == 2 && this.YLocation == 0)
                 {
                     this.FountainEnabled = true;
+                    Console.WriteLine("You have activated the Fountain of Objects!");
                     return true;
                 }
                 else
                 {
                     Console.WriteLine("nothing happens.");
+                    return false;
                 }
-
-                return true;
             default:
                 this.PrintValidActions();
                 return false;
         }
     }
 
-    private bool MovePlayer(int x, int y)
+    private bool MovePlayer(int x, int y, string direction)
     {
         // move x
         if (this.XLocation + x > 4 || this.XLocation + x < 0)
         {
+            Console.WriteLine($"You are at the edge and cannot move {direction}, choose another move");
             return false;
         }
         else
@@ -165,7 +139,7 @@ internal class Player
         // move y
         if (this.YLocation + y > 4 || this.YLocation + y < 0)
         {
-            Console.WriteLine($"current y{this.YLocation} -- to add {y}");
+            Console.WriteLine($"You are at the edge and cannot move {direction}, choose another move");
             return false;
         }
         else
