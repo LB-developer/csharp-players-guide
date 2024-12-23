bb6c96de5cab836d4b0408f174c0a0ee087c6d6d
lblmdesk <loganbendev@gmail.com>
2024-12-23 18:47:45 +1300

    Boss Battle (refactor) : The Fountain Of Objects

--- players-guide/Challenges/TheFountainOfObjects/Game.cs
+++ players-guide/Challenges/TheFountainOfObjects/Game.cs
@@ -28,7 +28,7 @@ public class Game
         this.Rooms[0, 2] = GameRoom.Fountain;
     }
 
-    public void StartGame()
+    private void StartGame()
     {
 
         while (!GameFinished)
@@ -49,7 +49,7 @@ public class Game
     private void StartNewRound()
     {
         Console.WriteLine($"---------------------------");
-        Console.WriteLine($"You are the room at (Row={Player.YLocation}, Column={Player.XLocation})");
+        Console.WriteLine($"You are the room at (Row={this.Player.YLocation}, Column={this.Player.XLocation})");
 
         if (this.Player.XLocation == 0 && this.Player.YLocation == 0)
             Console.WriteLine("You see light coming from the cavern entrance.");
@@ -65,101 +65,3 @@ public class Game
 
 }
 
-internal class Player
-{
-    internal int XLocation { get; private set; }
-    internal int YLocation { get; private set; }
-    internal bool FountainEnabled { get; private set; }
-
-    public Player(int x, int y)
-    {
-        XLocation = x;
-        YLocation = y;
-    }
-
-    internal void GetPlayerAction()
-    {
-        bool validInstruction = false;
-        do
-        {
-            Console.Write("What do you want to do?  ");
-            string? option;
-            do
-            {
-                option = Console.ReadLine();
-            } while (option == null);
-
-            validInstruction = ValidatePlayerAction(option!);
-
-        } while (!validInstruction);
-    }
-
-    private bool ValidatePlayerAction(string option)
-    {
-        switch (option)
-        {
-            case "move west":
-                return this.MovePlayer(-1, 0, "west");
-            case "move east":
-                return this.MovePlayer(1, 0, "east");
-            case "move north":
-                return this.MovePlayer(0, 1, "north");
-            case "move south":
-                return this.MovePlayer(0, -1, "south");
-            case "enable fountain":
-                if (this.XLocation == 2 && this.YLocation == 0)
-                {
-                    this.FountainEnabled = true;
-                    Console.WriteLine("You have activated the Fountain of Objects!");
-                    return true;
-                }
-                else
-                {
-                    Console.WriteLine("nothing happens.");
-                    return false;
-                }
-            default:
-                this.PrintValidActions();
-                return false;
-        }
-    }
-
-    private bool MovePlayer(int x, int y, string direction)
-    {
-        // move x
-        if (this.XLocation + x > 4 || this.XLocation + x < 0)
-        {
-            Console.WriteLine($"You are at the edge and cannot move {direction}, choose another move");
-            return false;
-        }
-        else
-        {
-            this.XLocation += x;
-        }
-        // move y
-        if (this.YLocation + y > 4 || this.YLocation + y < 0)
-        {
-            Console.WriteLine($"You are at the edge and cannot move {direction}, choose another move");
-            return false;
-        }
-        else
-        {
-            this.YLocation += y;
-        }
-
-        return true;
-    }
-
-    private void PrintValidActions()
-    {
-        Console.WriteLine("Valid actions ---");
-        Console.WriteLine("move north");
-        Console.WriteLine("move south");
-        Console.WriteLine("move east");
-        Console.WriteLine("move west");
-        if (this.XLocation == 2 && this.YLocation == 0)
-            Console.WriteLine("enable fountain");
-    }
-
-
-}
