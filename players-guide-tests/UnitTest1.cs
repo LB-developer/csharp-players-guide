2d8d416549a9a63cdcb4fe05f9e33593719e81af
lblmdesk <loganbendev@gmail.com>
2024-12-27 20:57:02 +1300

    Testing - Create first test, ensure projects are linked

---
+++ players-guide-tests/UnitTest1.cs
@@ -0,0 +1,17 @@
+﻿namespace players_guide_tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+        var sol = new Challenges.PartTwo.Sword();
+
+        var expected = 5;
+
+        var actual = sol._weight;
+
+        Assert.True(actual == expected);
+
+    }
+}
