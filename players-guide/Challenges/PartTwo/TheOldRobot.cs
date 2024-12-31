9575d266536226acdf424bef01da870dfd491c49
lblmdesk <loganbendev@gmail.com>
2024-12-31 21:55:01 +1300

    The Old Robot (refactor): user can add infinite commands

--- players-guide/Challenges/PartTwo/TheOldRobot.cs
+++ players-guide/Challenges/PartTwo/TheOldRobot.cs
@@ -14,31 +14,39 @@ public class Robot
     public void Run()
     {
         // Get user input and set to command
-        Commands[0] = new OnCommand();
+        Commands.Add(new OnCommand());
 
-        Console.WriteLine("Select 3 Commands from the list");
+        Console.WriteLine("Select Commands from the list");
         foreach (var option in Enum.GetValues<Options>())
         {
             Console.WriteLine($" > {option}");
         }
 
-
-        for (int i = 0; i < 3; i++)
+        bool enteringCommands = true;
+        do
         {
-            Console.Write($"Enter Option {i + 1}: ");
+            Console.Write($"Enter next option: ");
             var option = Console.ReadLine();
 
-            Commands[i] = option switch
+            if (option == "stop")
             {
-                "on" => new OnCommand(),
-                "off" => new OffCommand(),
-                "north" => new NorthCommand(),
-                "south" => new SouthCommand(),
-                "east" => new EastCommand(),
-                "west" => new WestCommand(),
-                _ => null
-            };
-        }
+                enteringCommands = false;
+                continue;
+            }
+            else
+            {
+                Commands.Add(option switch
+                {
+                    "on" => new OnCommand(),
+                    "off" => new OffCommand(),
+                    "north" => new NorthCommand(),
+                    "south" => new SouthCommand(),
+                    "east" => new EastCommand(),
+                    "west" => new WestCommand(),
+                    _ => null
+                });
+            }
+        } while (enteringCommands);
 
         foreach (IRobotCommand? command in Commands)
         {
